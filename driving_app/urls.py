0dbd1240ec42e5b3504e29bb52c680c9fef5cb8f
tonnymwanza <tonnimwanza@gmail.com>
2024-05-16 06:01:28 +0300

    working on registratio and login files

--- driving_app/urls.py
+++ driving_app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+
+from . import views
 from . views import HomeView
+from . views import AboutView
+from . views import ContactView
+from . views import CourseView
 # my urls
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
+    path('about_page', AboutView.as_view(), name='about_page'),
+    path('course', CourseView.as_view(), name='course'),
+    path('contact', ContactView.as_view(), name='contact'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
--- driving_app/views.py
+++ driving_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +12,54 @@ class HomeView(View):
 
     def get(self, request):
         return render(request, 'index.html')
+
+class AboutView(View):
+
+    def get(self, request):
+        return render(request, 'about.html')
+    
+class CourseView(View):
+
+    def get(self, request):
+        return render(request, 'courses.html')
+    
+class ContactView(View):
+
+    def get(self, request):
+        return render(request, 'contact.html')
+    
+def register(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username = username).exists():
+                messages.info(request, 'the username is taken')
+                return redirect('register')
+            else:
+                user  = User.objects.create_user(first_name = firstname, username = username, password = password)
+                return redirect('login')
+        else:
+            messages.error(request, 'the password dont match')
+            return redirect('register')
+    return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST[next])
+            else:
+                return redirect('index')
+        else:
+            messages.error(request, 'invalid credentials provided')
+            return redirect('login')
+    return render(request, 'login.html')
